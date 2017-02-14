018daf0135df60898a0e615abaf4c46536227a15
Wille <ville.ristimaki@nordsoftware.com>
2017-02-14 15:09:49 +0200

    Move connect-history-fallback inside dev

--- server/main.js
+++ server/main.js
@@ -7,15 +7,11 @@ const config = require('../config');
 const app = express();
 const paths = config.utils_paths;
 
-// This rewrites all routes requests to the root /index.html file
-// (ignoring file requests). If you want to implement universal
-// rendering, you'll want to remove this middleware.
-app.use(require('connect-history-api-fallback')());
-
 // ------------------------------------
 // Apply Webpack HMR Middleware
 // ------------------------------------
 if (config.env === 'development') {
+
   const compiler = webpack(webpackConfig);
 
   debug('Enable webpack dev and HMR middleware');
@@ -28,13 +24,20 @@ if (config.env === 'development') {
     lazy        : false,
     stats       : config.compiler_stats
   }));
+
   app.use(require('webpack-hot-middleware')(compiler));
+
+  // This rewrites all routes requests to the root /index.html file
+  // (ignoring file requests). If you want to implement universal
+  // rendering, you'll want to remove this middleware.
+  app.use(require('connect-history-api-fallback')());
 
   // Serve static assets from ~/src/static since Webpack is unaware of
   // these files. This middleware doesn't need to be enabled outside
   // of development since this directory will be copied into ~/dist
   // when the application is compiled.
   app.use(express.static(paths.client('static')));
+
 } else {
   debug(
     'Server is being run outside of live development mode, meaning it will ' +
