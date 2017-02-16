a0b8abe786b956c38cf121f7afbd419e26ec5a04
Wille <ville.ristimaki@nordsoftware.com>
2017-02-16 13:40:11 +0200

    Add container-className

--- src/components/Navigation/components/Navigation.js
+++ src/components/Navigation/components/Navigation.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { withRouter } from 'react-router';
-import './Navigation.scss';
 import InfinityMenu from 'react-infinity-menu';
+
+import './Navigation.scss';
 
 export class Navigation extends React.Component {
   constructor (props) {
@@ -41,30 +42,32 @@ export class Navigation extends React.Component {
 
   render () {
     let navigationTitle = 'Navigaatio';
-    // if (!this.props.is_open && this.props.selectedTOSPath.length > 0) {
-    //   navigationTitle = this.props.selectedTOSPath.map((section, index) => {
-    //     return <div key={index}>{section}</div>;
-    //   });
-    // }
+    if (!this.props.is_open && this.props.selectedTOS.tos.path.length > 0) {
+      navigationTitle = this.props.selectedTOS.tos.path.map((section, index) => {
+        return <div key={index}>{section}</div>;
+      });
+    }
 
     return (
-      <div className='navigation-menu'>
-        <button className='btn btn-default btn-sm pull-right' onClick={this.toggleNavigationVisibility}>
-          <span
-            className={'fa ' + (this.props.is_open ? 'fa-minus' : 'fa-plus')}
-            aria-hidden='true'
+      <div className='container-fluid'>
+        <div className='navigation-menu'>
+          <button className='btn btn-default btn-sm pull-right' onClick={this.toggleNavigationVisibility}>
+            <span
+              className={'fa ' + (this.props.is_open ? 'fa-minus' : 'fa-plus')}
+              aria-hidden='true'
+            />
+          </button>
+          {!this.props.is_open &&
+          <div className='nav-path-list' onClick={this.toggleNavigationVisibility}>{navigationTitle}</div>
+          }
+          {this.props.is_open &&
+          <InfinityMenu
+            tree={this.state.tree}
+            onNodeMouseClick={this.onNodeMouseClick}
+            onLeafMouseClick={this.onLeafMouseClick}
           />
-        </button>
-        {!this.props.is_open &&
-        <div className='nav-path-list' onClick={this.toggleNavigationVisibility}>{navigationTitle}</div>
-        }
-        {this.props.is_open &&
-        <InfinityMenu
-          tree={this.state.tree}
-          onNodeMouseClick={this.onNodeMouseClick}
-          onLeafMouseClick={this.onLeafMouseClick}
-        />
-        }
+          }
+        </div>
       </div>
     );
   }
