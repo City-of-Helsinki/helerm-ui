76e8abed4704e32512f3f3f16e39bd3a8e96a707
Joni Rämö <joni.ramo@nordsoftware.com>
2017-02-17 12:47:47 +0200

    Add a NotFound page to add 404

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 import CoreLayout from '../layouts/CoreLayout/CoreLayout';
 import HomeContainer from './UI/containers/HomeContainer';
 import ViewTOSContainer from './ViewTOS/containers/ViewTOSContainer';
+import NotFound from './NotFound/NotFound';
 
 export default () => (
-  <Route path='/' component={CoreLayout}>
-    <IndexRoute component={HomeContainer} />
-    <Route path='view-tos/:id' component={ViewTOSContainer} />
-  </Route>
+  <div>
+    <Route path='/' component={CoreLayout}>
+      <IndexRoute component={HomeContainer} />
+      <Route path='view-tos/:id' component={ViewTOSContainer} />
+    </Route>
+    <Route path='*' component={NotFound} />
+  </div>
 );
