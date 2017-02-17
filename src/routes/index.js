e3fb80c7f7129dc0d735f8128e5ba4df226911fe
Joni Rämö <joni.ramo@nordsoftware.com>
2017-02-17 11:02:26 +0200

    Rename Home in routes.js to HomeContainer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import CoreLayout from '../layouts/CoreLayout/CoreLayout';
-import Home from './UI/containers/HomeContainer';
+import HomeContainer from './UI/containers/HomeContainer';
 import ViewTOSContainer from './ViewTOS/containers/ViewTOSContainer';
 
 export default () => (
   <Route path='/' component={CoreLayout}>
-    <IndexRoute component={Home} />
+    <IndexRoute component={HomeContainer} />
     <Route path='view-tos/:id' component={ViewTOSContainer} />
   </Route>
 );
