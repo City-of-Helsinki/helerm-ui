3656d1522bb32b4d7eda012fe3d479d67d8ca24f
Wille <ville.ristimaki@nordsoftware.com>
2017-02-20 15:59:11 +0200

    Use Login on Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+
+import LoginContainer from '../Login/containers/LoginContainer';
+
 import './Header.scss';
 
 export const Header = () => (
   <nav className='navbar navbar-inverse container-fluid'>
     <a href='' className='brand-title navbar-brand'>Tiedonohjausjärjestelmä Alpha v0.1.4</a>
+    <LoginContainer />
   </nav>
 );
 
