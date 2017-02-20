9ee4cd25ff0f081c92a054fa50d9b6d370d02260
Joni Rämö <joni.ramo@nordsoftware.com>
2017-02-20 21:02:06 +0200

    Minify HomeView

--- src/routes/UI/components/HomeView.js
+++ src/routes/UI/components/HomeView.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './HomeView.scss';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export class HomeView extends React.Component {
   render () {
     return (
-      <div>
-        <ReactCSSTransitionGroup
-          transitionName={'alert-position'}
-          transitionEnterTimeout={1000}
-          transitionLeaveTimeout={600} />
-      </div>
+      <div/>
     );
   }
 }
