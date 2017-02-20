7622ecbfc41ac28913452c497def6d40d4def562
Wille <ville.ristimaki@nordsoftware.com>
2017-02-20 22:40:03 +0200

    Remove indexRoute, use CoreLayout as a base

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route } from 'react-router';
 import CoreLayout from '../layouts/CoreLayout/CoreLayout';
-import HomeContainer from './UI/containers/HomeContainer';
 import ViewTOSContainer from './ViewTOS/containers/ViewTOSContainer';
 import NotFound from './NotFound/NotFound';
 
 export default () => (
   <div>
     <Route path='/' component={CoreLayout}>
-      <IndexRoute component={HomeContainer} />
       <Route path='view-tos/:id' component={ViewTOSContainer} />
     </Route>
     <Route path='*' component={NotFound} />
