9402a6cc99e9c531e8284bd131176c18d87cc467
Wille <ville.ristimaki@nordsoftware.com>
2017-02-21 19:05:57 +0200

    Fallback to React router on dev & prod

--- server/main.js
+++ server/main.js
@@ -31,6 +31,8 @@ app.use('/auth', authRoutes);
 if (config.env === 'development') {
   const compiler = webpack(webpackConfig);
 
+  app.use(require('connect-history-api-fallback')());
+
   debug('Enable webpack dev and HMR middleware');
   app.use(require('webpack-dev-middleware')(compiler, {
     publicPath: webpackConfig.output.publicPath,
@@ -44,29 +46,20 @@ if (config.env === 'development') {
 
   app.use(require('webpack-hot-middleware')(compiler));
 
-  // This rewrites all routes requests to the root /index.html file
-  // (ignoring file requests). If you want to implement universal
-  // rendering, you'll want to remove this middleware.
-  app.use(require('connect-history-api-fallback')());
-
   // Serve static assets from ~/src/static since Webpack is unaware of
   // these files. This middleware doesn't need to be enabled outside
   // of development since this directory will be copied into ~/dist
   // when the application is compiled.
   app.use(express.static(paths.client('static')));
 } else {
-  debug(
-    'Server is being run outside of live development mode, meaning it will ' +
-    'only serve the compiled application bundle in ~/dist. Generally you ' +
-    'do not need an application server for this and can instead use a web ' +
-    'server such as nginx to serve your static files. See the "deployment" ' +
-    'section in the README for more information on deployment strategies.'
-  );
-
   // Serving ~/dist by default. Ideally these files should be served by
   // the web server and not the app server, but this helps to demo the
   // server in production.
   app.use(express.static(paths.dist()));
+
+  app.get('*', function (req, res) {
+    res.sendFile(`${paths.dist()}/index.html`);
+  });
 }
 
 export default app;
