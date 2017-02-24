eeac7d70a7a8021f530a8203666598432b888293
Wille <ville.ristimaki@nordsoftware.com>
2017-02-24 16:14:20 +0200

    Change text, import svg the "right" way

--- src/routes/NotFound/NotFound.js
+++ src/routes/NotFound/NotFound.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
 import './NotFound.scss';
+import hkiLogo from '../../static/assets/helsinki-vaakuna-helfisininen.svg';
 
 export const NotFound = () => (
   <div className='not-found-container'>
-    <img className='helsinki-logo' src={require('../../static/assets/helsinki-vaakuna-helfisininen.svg')} alt='Helsinki City Logo' />
+    <img className='helsinki-logo' src={hkiLogo} alt='Helsinki City Logo'/>
     <h1>Tätä sivua ei löytynyt :(</h1>
-    <p>Takaisin kotisivulle <Link to='/'>tästä.</Link></p>
+    <p>Takaisin etusivulle <Link to='/'>tästä.</Link></p>
   </div>
 );
 
