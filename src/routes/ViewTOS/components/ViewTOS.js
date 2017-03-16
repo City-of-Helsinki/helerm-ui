c620d3f8446d9b48fa3f2ab73ba6e301243aa078
Joni Rämö <joni.ramo@nordsoftware.com>
2017-03-16 10:55:56 +0200

    Close EditorForm if documentState is 'view'

--- src/routes/ViewTOS/components/ViewTOS.js
+++ src/routes/ViewTOS/components/ViewTOS.js
@@ -65,6 +65,9 @@ export class ViewTOS extends React.Component {
     if (route && route.path === 'view-tos/:id') {
       this.props.setNavigationVisibility(false);
     }
+    if (nextProps.selectedTOS.documentState === 'view') {
+      this.setState({ editingMetaData: false });
+    }
   }
 
   componentWillUnmount () {
