9a011ddd9268f56d83aac22ed917a8ddeee01106
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-03-16 14:50:31 +0200

    Split actionButtons & ActionButton

---
+++ src/components/TOS/Header/ActionButtons.js
@@ -0,0 +1,76 @@
+import React, { PropTypes } from 'react';
+
+import IsAllowed from 'components/IsAllowed/IsAllowed';
+import ActionButton from './ActionButton';
+
+import {
+  EDIT,
+  DRAFT,
+  SENT_FOR_REVIEW,
+  WAITING_FOR_APPROVAL,
+  APPROVED
+} from '../../../../config/constants';
+
+const ActionButtons = ({ cancelEdit, documentState, saveDraft, sendForInspection, setDocumentState, state }) => {
+  const editable = (
+    <div>
+      { documentState !== 'edit' &&
+      <IsAllowed to={EDIT}>
+        <span>
+          <ActionButton
+            className='btn-sm pull-right'
+            type='default'
+            action={sendForInspection}
+            label='Lähetä tarkastettavaksi'
+          />
+          <ActionButton
+            className='btn-sm pull-right'
+            type='primary'
+            action={() => setDocumentState('edit')}
+            label='Muokkaustila'
+          />
+        </span>
+      </IsAllowed>
+      }
+      {
+        documentState === 'edit' &&
+        <IsAllowed to={EDIT}>
+          <span>
+            <ActionButton
+              className='btn-sm pull-right'
+              type='primary'
+              action={saveDraft}
+              label='Tallenna luonnos'
+            />
+            <ActionButton
+              className='btn-sm pull-right'
+              type='danger'
+              action={cancelEdit}
+              label='Peruuta muokkaus'
+            />
+            <span
+              className='fa fa-asterisk required-asterisk required-legend'> = Pakollinen tieto
+            </span>
+          </span>
+        </IsAllowed>
+      }
+    </div>
+  );
+  
+  return (
+    <div>
+      { state === DRAFT && editable }
+    </div>
+  );
+};
+
+ActionButtons.propTypes = {
+  cancelEdit: PropTypes.func,
+  documentState: PropTypes.string,
+  saveDraft: PropTypes.func,
+  sendForInspection: PropTypes.func,
+  setDocumentState: PropTypes.func,
+  state: PropTypes.string.isRequired
+};
+
+export default ActionButtons;
