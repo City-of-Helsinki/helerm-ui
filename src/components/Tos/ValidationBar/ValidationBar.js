83aadb36b05d51fbeb9d35e644011df9384b7c88
Joni Rämö <joni.ramo@nordsoftware.com>
2017-03-22 10:13:42 +0200

    Create dummy component for ValidationBar

---
+++ src/components/Tos/ValidationBar/ValidationBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ValidationBar = () => {
+  return (
+    <div>
+      <h2>Puuttuvat metatiedot</h2>
+    </div>
+  );
+};
+
+export default ValidationBar;
