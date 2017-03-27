85f9cc043e28e379d30d066d0c2a1cdf8bf78243
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-03-27 13:38:32 +0300

    Add keyStrokeSupport-decorator

---
+++ src/decorators/key-stroke-support.js
@@ -0,0 +1,41 @@
+import React, { PropTypes, Component } from 'react';
+
+export default function (WrappedComponent) {
+  class KeyStrokeSupport extends Component {
+
+    static propTypes = {
+      cancel: PropTypes.func.isRequired,
+      submit: PropTypes.func.isRequired
+    };
+
+    constructor (props) {
+      super(props);
+      this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
+    componentWillMount () {
+      document.addEventListener('keydown', this.handleKeyDown);
+    }
+
+    componentWillUnmount () {
+      document.removeEventListener('keydown', this.handleKeyDown);
+    }
+
+    handleKeyDown (e) {
+      switch (e.keyCode) {
+        case 13:
+          this.props.submit();
+          break;
+        case 27:
+          this.props.cancel();
+          break;
+      }
+    }
+
+    render () {
+      return (<WrappedComponent {...this.props}/>);
+    }
+  }
+
+  return KeyStrokeSupport;
+};
