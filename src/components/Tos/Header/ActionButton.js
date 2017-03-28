9e5f9abd0cf30dc21c33de6b4125e239e9b924e6
Joni Rämö <joni.ramo@nordsoftware.com>
2017-03-28 15:06:54 +0300

    Expand ActionButton to take custom styles as a JSON object and an icon

--- src/components/Tos/Header/ActionButton.js
+++ src/components/Tos/Header/ActionButton.js
@@ -1,18 +1,23 @@
 import React, { PropTypes } from 'react';
 import classname from 'classnames';
 
-const ActionButton = ({ type, action, label, ...rest }) => (
+const ActionButton = ({ type, action, label, icon, style, ...rest }) => (
   <button
     disabled={rest.disabled}
+    style={style}
     className={classname('btn', `btn-${type}`, `${rest.className}`)}
     onClick={action}>
+    <span className={icon}/>
+    {' '}
     {label}
   </button>
 );
 
 ActionButton.propTypes = {
   action: PropTypes.func.isRequired,
+  icon: PropTypes.string,
   label: PropTypes.string.isRequired,
+  style: PropTypes.object,
   type: PropTypes.string
 };
 
