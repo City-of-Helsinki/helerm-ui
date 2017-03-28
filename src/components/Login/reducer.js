d2eb6d52c2e42378e7c4fbb2fecec95f88b59ee7
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-03-28 18:10:39 +0300

    Remove redundand import

--- src/components/Login/reducer.js
+++ src/components/Login/reducer.js
@@ -3,7 +3,7 @@ import update from 'immutability-helper';
 import { createAction, handleActions } from 'redux-actions';
 import { isEmpty, get, isString } from 'lodash';
 
-import { centeredPopUp } from '../../utils/helpers';
+// import { centeredPopUp } from '../../utils/helpers';
 import { default as api } from '../../utils/api';
 import { setStorageItem, removeStorageItem } from '../../utils/storage';
 
