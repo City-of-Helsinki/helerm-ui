c8b0956c50b0879c0ae7636fea5d0f216b3d64db
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-03-29 14:01:05 +0300

    Add infinityMenu as a Component

---
+++ src/components/InfinityMenu/infinityMenu.js
@@ -0,0 +1,368 @@
+import React, { PropTypes } from 'react';
+import SearchInput from './searchInput';
+import NestedObjects from 'nested-objects';
+import _get from 'lodash/get';
+
+export default class InfinityMenu extends React.Component {
+
+  static propTypes = {
+    customComponentMappings: PropTypes.object,
+    disableDefaultHeaderContent: PropTypes.bool,
+    emptyTreeComponent: PropTypes.any,
+    emptyTreeComponentProps: PropTypes.object,
+    filter: PropTypes.func,
+    headerContent: PropTypes.any,
+    headerProps: PropTypes.object,
+    maxLeaves: PropTypes.number,
+    onLeafMouseClick: PropTypes.func,
+    onLeafMouseDown: PropTypes.func,
+    onLeafMouseUp: PropTypes.func,
+    onNodeMouseClick: PropTypes.func,
+    placeholder: PropTypes.string,
+    tree: PropTypes.array
+  };
+
+  static defaultProps = {
+    disableDefaultHeaderContent: false,
+    emptyTreeComponent: null,
+    emptyTreeComponentProps: {},
+    filter: (node, searchInput) => node.name.toLowerCase().indexOf(searchInput.toLowerCase()) >= 0,
+    headerContent: null,
+    headerProps: {},
+    maxLeaves: Infinity,
+    onLeafMouseClick: () => {},
+    onLeafMouseDown: () => {},
+    onLeafMouseUp: () => {},
+    onNodeMouseClick: () => {},
+    tree: []
+  };
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      search: {
+        isSearching: false,
+        searchInput: ''
+      }
+    };
+    this.setSearchInput = this.setSearchInput.bind(this);
+    this.stopSearching = this.stopSearching.bind(this);
+    this.startSearching = this.startSearching.bind(this);
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    if (nextProps.shouldComponentUpdate) {
+      return nextProps.shouldComponentUpdate(this.props, this.state, nextProps, nextState);
+    }
+    else {
+      return true;
+    }
+  }
+
+  onNodeClick (tree, node, keyPath, event) {
+    event.preventDefault();
+    if (!this.state.search.isSearching || !this.state.search.searchInput.length) {
+      node.isOpen = !node.isOpen;
+      node.maxLeaves = this.props.maxLeaves;
+      NestedObjects.set(tree, keyPath, node);
+      if (this.props.onNodeMouseClick) {
+        const currLevel = Math.floor(keyPath.split('.').length / 2);
+        this.props.onNodeMouseClick(event, tree, node, currLevel, keyPath);
+      }
+    }
+  }
+
+  onLoadMoreClick (tree, node, keyPath, event) {
+    event.preventDefault();
+    // get parent node so we can increment it's unique max leaves property
+    const keyPathArray = keyPath.split('.');
+    const parentPath = Object.assign([], keyPathArray).splice(0, keyPathArray.length - 2);
+    const parentNode = _get(this.props.tree, parentPath);
+    // set new max leaves - if none exist use component default property
+    parentNode.maxLeaves = (!parentNode.maxLeaves) ? this.props.maxLeaves : parentNode.maxLeaves + this.props.maxLeaves;
+    if (this.props.onNodeMouseClick) {
+      const currLevel = Math.floor(keyPath.split('.').length / 2);
+      this.props.onNodeMouseClick(event, tree, node, currLevel, keyPath);
+    }
+  }
+
+  startSearching () {
+    this.setState({
+      search: {
+        isSearching: true,
+        searchInput: ''
+      }
+    });
+  }
+
+  stopSearching () {
+    this.setState({
+      search: {
+        isSearching: false,
+        searchInput: ''
+      }
+    });
+  }
+
+  setSearchInput (event) {
+    this.setState({
+      search: {
+        isSearching: true,
+        searchInput: event.target.value
+      }
+    });
+  }
+
+  findFiltered (trees, node, key) {
+    if (!node.children) {
+      const nodeMatchesSearchFilter = this.props.filter(node, this.state.search.searchInput);
+      if (nodeMatchesSearchFilter) {
+        node.isSearchDisplay = true;
+        trees[key] = node;
+        return trees;
+      }
+      else {
+        node.isSearchDisplay = false;
+        trees[key] = node;
+        return trees;
+      }
+    }
+    else {
+      const filteredSubFolder = node.children.length ? node.children.reduce((p, c, k) => {
+        return this.findFiltered(p, c, k);
+      }, []) : [];
+      const shouldDisplay = filteredSubFolder.some(child => child.isSearchDisplay);
+
+      if (shouldDisplay) {
+        node.isSearchOpen = true;
+        node.children = filteredSubFolder;
+        node.isSearchDisplay = true;
+        node.maxLeaves = (node.maxLeaves) ? node.maxLeaves : this.props.maxLeaves;
+        trees[key] = node;
+        return trees;
+      }
+      else {
+        node.isSearchOpen = false;
+        node.isSearchDisplay = false;
+        trees[key] = node;
+        return trees;
+      }
+    }
+  }
+
+  setDisplayTree (tree, prevs, curr, keyPath) {
+    const currLevel = Math.floor(keyPath.length / 2);
+    const currCustomComponent = typeof curr.customComponent === 'string' ? this.props.customComponentMappings[curr.customComponent] : curr.customComponent;
+    const currCustomloadMoreComponent = (this.props.loadMoreComponent) ? this.props.loadMoreComponent : null;
+    const isSearching = this.state.search.isSearching && this.state.search.searchInput;
+    const shouldDisplay = (isSearching && curr.isSearchDisplay) || !isSearching;
+    curr.keyPath = keyPath;
+
+    // the leaves
+    if (!curr.children) {
+      const keyPathArray = keyPath.split('.');
+      const parentPath = Object.assign([], keyPathArray).splice(0, keyPathArray.length - 2);
+      const parentNode = _get(this.props.tree, parentPath);
+      const filteredChildren = (
+        parentNode.children.some(child => child.isSearchDisplay === true)
+          ? parentNode.children.filter(child => child.isSearchDisplay === true)
+          : parentNode.children
+      );
+      const itemKey = 'infinity-menu-leaf-' + curr.id;
+      const visIds = filteredChildren.map((e) => e.id);
+
+      let relativeIndex = visIds.indexOf(curr.id);
+      relativeIndex = (relativeIndex === -1) ? Infinity : relativeIndex;
+
+      let parentMaxLeaves = parentNode.maxLeaves || this.props.maxLeaves;
+      if (shouldDisplay && parentMaxLeaves > relativeIndex) {
+        if (curr.customComponent) {
+          const componentProps = {
+            key: itemKey,
+            onMouseDown: (e) => {
+              this.props.onLeafMouseDown ? this.props.onLeafMouseDown(e, curr) : null;
+            },
+            onMouseUp: (e) => {
+              this.props.onLeafMouseUp ? this.props.onLeafMouseUp(e, curr) : null;
+            },
+            onClick: (e) => {
+              this.props.onLeafMouseClick ? this.props.onLeafMouseClick(e, curr) : null;
+            },
+            name: curr.name,
+            icon: curr.icon,
+            data: curr
+          };
+          prevs.push(React.createElement(currCustomComponent, componentProps));
+        }
+        else {
+          prevs.push(
+            <li key={itemKey}
+                className='infinity-menu-leaf-container'
+                onMouseDown={(e) => this.props.onLeafMouseDown ? this.props.onLeafMouseDown(e, curr) : null}
+                onMouseUp={(e) => this.props.onLeafMouseUp ? this.props.onLeafMouseUp(e, curr) : null}
+                onClick={(e) => this.props.onLeafMouseClick ? this.props.onLeafMouseClick(e, curr) : null}
+            >
+              <span>{curr.name}</span>
+            </li>
+          );
+        }
+      }
+      else {
+        if (relativeIndex === filteredChildren.length - 1) {
+          if (currCustomloadMoreComponent) {
+            const loadMoreProps = {
+              key: itemKey,
+              onClick: this.onLoadMoreClick.bind(this, tree, curr, keyPath)
+            };
+            prevs.push(React.createElement(currCustomloadMoreComponent, loadMoreProps));
+          }
+          else {
+            prevs.push(
+              <li key={itemKey}
+                  className='infinity-menu-load-more-container'
+                  onClick={this.onLoadMoreClick.bind(this, tree, curr, keyPath)}
+              >
+                <span>Load more</span>
+              </li>
+            );
+          }
+        }
+      }
+      return prevs;
+    }
+    // the node
+    else {
+      const key = 'infinity-menu-node-' + currLevel + '-' + curr.id;
+      const nodeName = curr.name;
+      if ((!curr.isOpen && !isSearching) || (!curr.isSearchOpen && isSearching)) {
+        if (shouldDisplay) {
+          if (curr.customComponent) {
+            const nodeProps = {
+              onClick: this.onNodeClick.bind(this, tree, curr, keyPath),
+              name: nodeName,
+              isOpen: curr.isOpen,
+              isSearching: false,
+              data: curr,
+              key
+            };
+            prevs.push(React.createElement(currCustomComponent, nodeProps));
+          }
+          else {
+            prevs.push(
+              <div key={key}
+                   onClick={this.onNodeClick.bind(this, tree, curr, keyPath)}
+                   className='infinity-menu-node-container'
+              >
+                <label>{nodeName}</label>
+              </div>
+            );
+          }
+        }
+        return prevs;
+      }
+      else {
+        let openedNode = [];
+        if (shouldDisplay) {
+          if (curr.customComponent) {
+            const nodeProps = {
+              onClick: this.onNodeClick.bind(this, tree, curr, keyPath),
+              name: nodeName,
+              isOpen: curr.isOpen,
+              data: curr,
+              key,
+              isSearching
+            };
+            openedNode.push(React.createElement(currCustomComponent, nodeProps));
+          }
+          else {
+            openedNode.push(
+              <div key={key}
+                   onClick={this.onNodeClick.bind(this, tree, curr, keyPath)}
+                   className='infinity-menu-node-container'
+              >
+                <label>{nodeName}</label>
+              </div>
+            );
+          }
+
+          const childrenList = curr.children.length ? curr.children.reduce((p, c, k) => {
+            if (c === undefined || k === undefined) {
+              return p;
+            }
+            return this.setDisplayTree(tree, p, c, keyPath + '.children.' + k);
+          }, []) : [];
+
+          if (childrenList.length > 0) {
+            openedNode.push(
+              <ul key={'infinity-menu-children-list' + currLevel}>
+                {childrenList}
+              </ul>
+            );
+          }
+          prevs.push(openedNode);
+        }
+        return prevs;
+      }
+    }
+  }
+
+  renderBody (displayTree) {
+    const {
+      emptyTreeComponent,
+      emptyTreeComponentProps
+    } = this.props;
+
+    if (displayTree.length) {
+      return displayTree;
+    }
+    else if (emptyTreeComponent) {
+      return React.createElement(emptyTreeComponent, emptyTreeComponentProps);
+    }
+    else {
+      return null;
+    }
+  }
+
+  render () {
+    const tree = this.props.tree;
+    // find filtered folders base on search, if there no search, return all
+    const filteredTree = this.state.search.isSearching && this.state.search.searchInput ? tree.reduce((prev, curr, key) => {
+      if (key === undefined) {
+        return prev;
+      }
+      return this.findFiltered(prev, curr, key);
+    }, []) : tree;
+
+    // recursive go through the tree
+    const displayTree = filteredTree.reduce((prev, curr, key) => {
+      if (key === undefined) {
+        return prev;
+      }
+      return this.setDisplayTree(tree, prev, curr, key.toString());
+    }, []);
+
+    // header component
+    const headerProps = {
+      ...this.props.headerProps,
+      isSearching: this.state.search.isSearching,
+      placeholder: this.props.placeholder,
+      searchInput: this.state.search.searchInput,
+      setSearchInput: this.setSearchInput,
+      startSearching: this.startSearching,
+      stopSearching: this.stopSearching
+    };
+
+    const bodyContent = this.renderBody(displayTree);
+    const defaultHeaderContent = this.props.disableDefaultHeaderContent ? null : React.createElement(SearchInput, headerProps);
+    const headerContent = this.props.headerContent ? React.createElement(this.props.headerContent, headerProps) : defaultHeaderContent;
+
+    return (
+      <div className='infinity-menu-container'>
+        {headerContent}
+        <div className='infinity-menu-display-tree-container'>
+          {bodyContent}
+        </div>
+      </div>
+    );
+  }
+};
