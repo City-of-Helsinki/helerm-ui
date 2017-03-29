b9f4d00e957cddf56c8ce2b1b1ba1c6cb664dd09
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-03-29 10:18:56 +0300

    Add optional icon to header actionbutton

--- src/components/Tos/Header/ActionButton.js
+++ src/components/Tos/Header/ActionButton.js
@@ -6,7 +6,7 @@ const ActionButton = ({ type, action, label, ...rest }) => (
     disabled={rest.disabled}
     className={classname('btn', `btn-${type}`, `${rest.className}`)}
     onClick={action}>
-    {label}
+    {rest.icon && <i className={classname('fa', `${rest.icon}`)}></i>} {label}
   </button>
 );
 
