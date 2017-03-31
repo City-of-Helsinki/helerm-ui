048129245e682aee7878bbe3c5228d7504f8cdb7
Wille Ristimäki <ville.ristimaki@nordsoftware.com>
2017-03-31 16:49:15 +0300

    Working implementation of clone single tos

--- src/components/Tos/CloneView/CloneView.js
+++ src/components/Tos/CloneView/CloneView.js
@@ -1,7 +1,11 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, createElement } from 'react';
 import classNames from 'classnames';
 
 import './CloneView.scss';
+import NavigationContainer from '../../Navigation/NavigationContainer';
+
+const METHOD_TEMPLATE = 'template';
+const METHOD_FUNCTION = 'function';
 
 export default class CloneView extends React.Component {
 
@@ -16,9 +20,11 @@ export default class CloneView extends React.Component {
 
     this.cloneFromTemplate = this.cloneFromTemplate.bind(this);
     this.selectItem = this.selectItem.bind(this);
+    this.selectMethod = this.selectMethod.bind(this);
 
     this.state = {
-      selectedItem: null
+      selectedItem: {},
+      selectedMethod: METHOD_TEMPLATE
     };
   }
 
@@ -28,35 +34,65 @@ export default class CloneView extends React.Component {
     return cloneFromTemplate(id);
   }
 
-  selectItem (key) {
-    this.setState({ selectedItem: key });
+  selectItem ({ id, name }) {
+    this.setState({ selectedItem: { id, name } });
+  }
+
+  selectMethod (selectedMethod) {
+    return this.setState({ selectedMethod });
   }
 
   render () {
     const { toggleCloneView, templates } = this.props;
-    const { selectedItem } = this.state;
+    const { selectedItem, selectedMethod } = this.state;
+
+    const treeViewProps = {
+      tosPath: [],
+      onLeafMouseClick: (event, leaf) => {
+        const { id, name } = leaf;
+        return this.selectItem({ id, name });
+      }
+    };
+
+    const treeView = createElement(NavigationContainer, treeViewProps);
+
     return (
       <div className='row clone__view'>
-        <h3>Tuo kuvaus</h3>
-        <h5>Valitse listalta tuotava kuvaus</h5>
-        <div className='importable-elements '>
+        <ul className='nav nav-tabs'>
+          <li role='presentation' className={classNames({ 'active': selectedMethod === METHOD_TEMPLATE })}>
+            <a onClick={() => this.selectMethod(METHOD_TEMPLATE)}>Tuo kuvaus templatesta</a>
+          </li>
+          <li role='presentation' className={classNames({ 'active': selectedMethod === METHOD_FUNCTION })}>
+            <a onClick={() => this.selectMethod(METHOD_FUNCTION)}>Tuo kuvaus kuvauksesta</a>
+          </li>
+        </ul>
+
+        {selectedMethod === METHOD_TEMPLATE &&
+        <div className='importable-elements'>
           <div className='list-group'>
             {templates.map(({ name, id }) => (
               <button key={id}
                       type='button'
-                      className={classNames('list-group-item', { 'active': selectedItem === id })}
-                      onClick={() => this.selectItem(id)}>
+                      className={classNames('list-group-item', { 'active': selectedItem.id === id })}
+                      onClick={() => this.selectItem({ id, name })}>
                 {name}
               </button>
             ))}
           </div>
         </div>
+        }
+
+        {selectedMethod === METHOD_FUNCTION && <div className='row'>{treeView}</div>}
+
         <div className='clone-controls clearfix'>
-          <div className='alert alert-danger' role='alert'><strong>Huom!</strong> Aiemmat tiedot korvataan.</div>
+          {selectedItem.id &&
+          <div className='alert alert-danger' role='alert'><strong>Huom!</strong> Aiemmat tiedot korvataan
+            kuvauksella {selectedItem.name}.</div>
+          }
           <button
-            onClick={() => this.cloneFromTemplate(selectedItem)}
+            onClick={() => this.cloneFromTemplate(selectedItem.id)}
             className='btn btn-primary pull-right'
-            disabled={!selectedItem}>
+            disabled={!selectedItem.id}>
             Tuo
           </button>
           <button onClick={toggleCloneView} className='btn btn-danger pull-right'>Peruuta</button>
