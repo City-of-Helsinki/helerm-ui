ae02e3b5c132a43bc4f84c5bde144fe9232932cc
Ville Vaarala <ville.l.vaarala@gmail.com>
2017-12-04 11:36:40 +0200

    Fix logout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,25 +6,23 @@ const router = express.Router();
 /**
  * GET /auth/login/helsinki
  */
-router.route('/login/helsinki')
-  .get(authCtrl.passport.authenticate('helsinki'));
+router.route('/login/helsinki').get(authCtrl.passport.authenticate('helsinki'));
 
 /**
  * GET /auth/login/helsinki/return
  */
-router.route('/login/helsinki/return')
+router
+  .route('/login/helsinki/return')
   .get(authCtrl.passport.authenticate('helsinki'), authCtrl.authCallback);
 
 /**
  * GET /auth/me
  */
-router.route('/me')
-  .get(authCtrl.getCurrentUser);
+router.route('/me').get(authCtrl.getCurrentUser);
 
 /**
  * POST /auth/logout
  */
-router.route('/logout')
-  .post(authCtrl.logOut);
+router.route('/logout').get(authCtrl.logOut);
 
 export default router;
