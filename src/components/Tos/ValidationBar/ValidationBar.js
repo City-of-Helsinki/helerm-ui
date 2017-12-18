4fc823dce61af3990574b6b6fe11ba911d7019be
Titta Riiheläinen <titta.riihelainen@digia.com>
2017-12-18 17:53:56 +0200

    Warn type validation errors. Filtering validation errors by type.

--- src/components/Tos/ValidationBar/ValidationBar.js
+++ src/components/Tos/ValidationBar/ValidationBar.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import SideBar from 'react-sidebar';
-import map from 'lodash/map';
-import filter from 'lodash/filter';
+import Select from 'react-select';
+import {map, filter, find } from 'lodash';
 import './ValidationBar.scss';
 
 import {
   validateTOS,
   validatePhase,
   validateAction,
-  validateRecord
+  validateRecord,
+  validateTOSWarnings,
+  validatePhaseWarnings,
+  validateActionWarnings,
+  validateRecordWarnings
 } from '../../../utils/validators';
 
 const styles = {
@@ -19,7 +23,7 @@ const styles = {
     position: 'fixed',
     width: 300,
     height: '100%',
-    padding: '10px 0 0 20px',
+    padding: '10px 10px 0 20px',
     backgroundColor: 'white',
     zIndex: '99'
   },
@@ -40,34 +44,58 @@ const styles = {
   }
 };
 
+const FILTER_VALUE_MISSING = 'missing';
+const FILTER_VALUE_WARN = 'warn';
+
+const filterStatuses = [
+  { value: FILTER_VALUE_MISSING, label: 'Puuttuvat' },
+  { value: FILTER_VALUE_WARN, label: 'Virheelliset' }
+];
+
 export class ValidationBar extends Component {
   constructor (props) {
     super(props);
     this.renderContent = this.renderContent.bind(this);
+    this.onFilterStatusChange = this.onFilterStatusChange.bind(this);
+    this.state = {
+      filterStatus: []
+    };
   }
 
-  generateInvalidAttributes (validate, values) {
-    const invalidAttributes = validate(values, this.props.attributeTypes);
+  generateInvalidAttributes (validate, validateWarn, values) {
+    const showInvalidAttributes = this.getFilterByStatus(FILTER_VALUE_MISSING);
+    const showWarnAttributes = this.getFilterByStatus(FILTER_VALUE_WARN);
+    const invalidAttributes = showInvalidAttributes ? validate(values, this.props.attributeTypes) : [];
+    const warnAttributes = showWarnAttributes ? validateWarn(values, this.props.attributeTypes) : [];
     const mappedInvalidAttributes = map(invalidAttributes, (item, index) => (
       <div key={index} className='missing-attribute'>
-        {'• '}{this.props.attributeTypes[item].name}
+        <i className='fa fa-times-circle'></i> {this.props.attributeTypes[item].name}
+      </div>
+    ));
+    const mappedWarnAttributes = map(warnAttributes, (item, index) => (
+      <div key={index} className='warn-attribute'>
+        <i className='fa fa-exclamation-circle'></i> {this.props.attributeTypes[item].name}
       </div>
     ));
 
-    if (invalidAttributes.length) {
+    if (invalidAttributes.length || warnAttributes.length) {
       return (
         <div className='missing-attributes'>
           {mappedInvalidAttributes}
+          {mappedWarnAttributes}
         </div>
       );
     }
   }
 
-  generateAttributeSection (validate, elements) {
+  generateAttributeSection (validateRequired, validateWarn, elements) {
     const { attributeTypes } = this.props;
+    const showInvalidAttributes = this.getFilterByStatus(FILTER_VALUE_MISSING);
+    const showWarnAttributes = this.getFilterByStatus(FILTER_VALUE_WARN);
     const mappedAttributeSections = map(elements, (element, index) => {
-      const invalidAttributes = validate(element, attributeTypes);
-      if (invalidAttributes.length) {
+      const invalidAttributes = showInvalidAttributes ? validateRequired(element, attributeTypes) : [];
+      const warnAttributes = showWarnAttributes ? validateWarn(element, attributeTypes) : [];
+      if (invalidAttributes.length || warnAttributes.length) {
         return (
           <div key={index}>
             <div className='parent-name'>{element.name}</div>
@@ -75,7 +103,14 @@ export class ValidationBar extends Component {
               {map(invalidAttributes, (item, key) => {
                 return (
                   <div key={key} className='missing-attribute'>
-                    {'•'} {attributeTypes[item].name}
+                    <i className='fa fa-times-circle'></i> {attributeTypes[item].name}
+                  </div>
+                );
+              })}
+              {map(warnAttributes, (item, key) => {
+                return (
+                  <div key={key} className='warn-attribute'>
+                    <i className='fa fa-exclamation-circle'></i> {attributeTypes[item].name}
                   </div>
                 );
               })}
@@ -88,21 +123,33 @@ export class ValidationBar extends Component {
     return filter(mappedAttributeSections, (section) => (section !== undefined));
   }
 
-  renderContent () {
-    const { selectedTOS } = this.props;
+  onFilterStatusChange (options) {
+    this.setState({
+      filterStatus: map(options, 'value')
+    });
+  }
 
-    const invalidTOSAttributes = this.generateInvalidAttributes(validateTOS, selectedTOS);
-    const invalidPhaseAttributes = this.generateAttributeSection(validatePhase, selectedTOS.phases);
-    const invalidActionAttributes = this.generateAttributeSection(validateAction, selectedTOS.actions);
-    const invalidRecordAttributes = this.generateAttributeSection(validateRecord, selectedTOS.records);
+  getFilterByStatus (filterValue) {
+    if (!this.state.filterStatus.length) {
+      return true;
+    }
+    return find(this.state.filterStatus, (item) => (item === filterValue)) ? true : false;
+  }
+
+  renderInvalidContent () {
+    const { selectedTOS } = this.props;
+    const invalidTOSAttributes = this.generateInvalidAttributes(validateTOS, validateTOSWarnings, selectedTOS);
+    const invalidPhaseAttributes = this.generateAttributeSection(validatePhase, validatePhaseWarnings, selectedTOS.phases);
+    const invalidActionAttributes = this.generateAttributeSection(validateAction, validateActionWarnings, selectedTOS.actions);
+    const invalidRecordAttributes = this.generateAttributeSection(validateRecord, validateRecordWarnings, selectedTOS.records);
 
     if (invalidTOSAttributes ||
         invalidPhaseAttributes.length > 0 ||
         invalidActionAttributes.length > 0 ||
         invalidRecordAttributes.length > 0) {
       return (
-        <div className='sidebar-content'>
-          <h4>Puuttuvat metatiedot</h4>
+        <div>
+          <h4>Puutteelliset metatiedot</h4>
           {invalidTOSAttributes &&
             <h5>Asian metatiedot</h5>}
           {invalidTOSAttributes}
@@ -119,8 +166,25 @@ export class ValidationBar extends Component {
       );
     }
     return (
+      <div className='no-missing-attributes fa fa-check-circle'/>
+    );
+  }
+
+  renderContent () {
+    const invalidContent = this.renderInvalidContent();
+    return (
       <div className='sidebar-content'>
-        <div className='no-missing-attributes fa fa-check-circle'/>
+        <Select
+          autoBlur={true}
+          placeholder='Suodata tyypin mukaan...'
+          value={this.state.filterStatus}
+          multi={true}
+          joinValues={true}
+          clearable={false}
+          options={filterStatuses}
+          onChange={this.onFilterStatusChange}
+        />
+        {invalidContent}
       </div>
     );
   }
