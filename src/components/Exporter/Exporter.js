7b4d554a81cd75f6352b9f13485c963cc4057c7f
Ville Ristimäki <villeristimaki@gmail.com>
2018-09-16 23:18:08 +0300

    Working Exporter

--- src/components/Exporter/Exporter.js
+++ src/components/Exporter/Exporter.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import isArray from 'lodash/isArray';
+import { CSVLink } from 'react-csv';
 
-const createExport = (data) => {
-  // Tehtäväluokka – käsittelyvaihe – toimenpide – asiakirja
-  //    - attributes
-  // Do something with the data...
+const getChildren = (array, item) => {
+  if (isArray(item.children)) {
+    return item.children.reduce(getChildren, array);
+  }
+
+  const exportItem = {
+    name: item.title,
+    status: item.function_state,
+    ...item.function_attributes
+  };
+
+  array.push(exportItem);
+  return array;
 };
 
 const Exporter = ({ data }) => {
-  const countStr = data.length > 1 ? 'tulosta' : 'tulos';
+  const exportData = data.reduce(getChildren, []);
+  const countStr = exportData.length > 1 ? 'tulosta' : 'tulos';
+  const fileName = `helerm-export_${new Date().getTime()}.csv`;
+
   return (
-    <button className='btn btn-primary' onClick={() => createExport(data)}>Vie {data.length} {countStr} <i className='fa fa-file-excel-o' /></button>
+    <CSVLink data={exportData}
+      filename={fileName}
+      className='btn btn-primary'
+      target='_blank'>
+        Vie {exportData.length} {countStr} <i className='fa fa-file-excel-o' />
+    </CSVLink>
   );
 };
 
