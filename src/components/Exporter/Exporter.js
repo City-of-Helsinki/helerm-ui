75779d0b32d96469bc602c25914f36cee5d95037
Ville Ristimäki <villeristimaki@gmail.com>
2018-09-28 16:45:13 +0300

    WIP: Create (just the function for now) xlsx-sheet for export

--- src/components/Exporter/Exporter.js
+++ src/components/Exporter/Exporter.js
@@ -2,21 +2,59 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import isArray from 'lodash/isArray';
-import { CSVLink } from 'react-csv';
+import XLSX from 'xlsx';
 
 const getChildren = (array, item) => {
   if (isArray(item.children)) {
     return item.children.reduce(getChildren, array);
   }
 
+  // XX XX XX Käsittelyprosessi (function)
+  //    => function_attributes
+
+  // käsittelyvaihe
+  //    => function_attributes
+  // toimenpide
+  //    => function_attributes
+  // Asiakirja
+  //    => function_attributes
+  // Asiakirja
+  //    => function_attributes
+  // Asiakirja
+  //    => function_attributes
+  // ...
   const exportItem = {
+    code: item.code,
     name: item.title,
-    status: item.function_state,
-    ...item.function_attributes
+    attributes: item.function_attributes
   };
 
   array.push(exportItem);
   return array;
+};
+
+const createWorkBook = (filename, items) => {
+  const workBook = XLSX.utils.book_new();
+
+  // Add two blank, merge attributes of each items & sort alphabetically
+  const headers = [null, null, ...items.map(({ attributes }) => Object.keys(attributes)).reduce((a, b) => [...new Set([...a, ...b])], []).sort()];
+
+  items.forEach(({ name, attributes, code }) => {
+    const data = [];
+    // TODO: Just käsittelyprosessi for now, add the phases & actions from loop
+    data[0] = 'käsittelyprosessi';
+    data[1] = `${code} ${name}`;
+
+    Object.keys(attributes).forEach((attrName) => {
+      const indexInHeaders = headers.indexOf(attrName);
+      data[indexInHeaders] = attributes[attrName];
+    });
+
+    const workSheet = XLSX.utils.aoa_to_sheet([headers, data]);
+    XLSX.utils.book_append_sheet(workBook, workSheet, code);
+  });
+
+  XLSX.writeFile(workBook, filename);
 };
 
 const Exporter = ({ data, className, isVisible }) => {
@@ -26,15 +64,15 @@ const Exporter = ({ data, className, isVisible }) => {
 
   const exportData = data.reduce(getChildren, []);
   const countStr = exportData.length > 1 ? 'tulosta' : 'tulos';
-  const fileName = `helerm-export_${new Date().getTime()}.csv`;
+  const fileName = `helerm-export_${new Date().getTime()}.xlsx`;
 
   return (
-    <CSVLink data={exportData}
-      filename={fileName}
+    <button
       className={classnames('btn btn-primary', className)}
-      target='_blank'>
-        Vie {exportData.length} {countStr} <i className='fa fa-file-excel-o' />
-    </CSVLink>
+      target='_blank'
+      onClick={() => createWorkBook(fileName, exportData)}>
+      Vie {exportData.length} {countStr} <i className='fa fa-file-excel-o' />
+    </button>
   );
 };
 
