527c3c65fc7a62c76f3a070db20b7ecb88cac43f
Arto Pärssinen <arto.parssinen@gofore.com>
2023-03-31 14:33:08 +0300

    Added tests for ToS Action component

---
+++ src/components/Tos/Action/__tests__/Action.spec.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-underscore-dangle */
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Enzyme, { mount } from 'enzyme';
+import React from 'react';
+
+import Action from '../Action';
+
+import { getBaseValues } from '../../../../utils/helpers';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('(Component) Action', () => {
+    let _wrapper;
+    const attributeTypes = { TypeSpecifier: {defaultIn: []} }
+    const actionRecords = {
+        row1: 'testi1',
+        row2: 'testi2',
+        row3: 'testi3',
+    }
+    const records = {
+        testi1: {},
+        testi2: {}
+    }
+    const testFunc = () => { return }
+    beforeEach(() => {
+        _wrapper = mount(
+        <Action
+            action={{attributes: {showAttributes: false}, is_open: true, records: []}}
+            actionTypes={{}}
+            actions={{records:records}}
+            addRecord={testFunc}
+            attributeTypes={attributeTypes}
+            changeOrder={testFunc}
+            displayMessage={testFunc}
+            documentState="test"
+            editAction={testFunc}
+            editActionAttribute={testFunc}
+            editRecord={testFunc}
+            editRecordAttribute={testFunc}
+            importItems={testFunc}
+            phases={{}}
+            phasesOrder={[]}
+            recordTypes={{}}
+            records={records}
+            removeAction={testFunc}
+            removeRecord={testFunc}
+            setActionVisibility={testFunc}
+            setRecordVisibility={testFunc}
+        />
+        );
+    });
+
+    it('should render without crashing', () => {
+        expect(_wrapper).toBeDefined()
+    });
+    
+    it('should have correct state when creating or canceling new record creation', () => {
+        const inst = _wrapper.instance();
+        expect(inst.state.creatingRecord).toEqual(false);
+        inst.createNewRecord();
+        expect(inst.state.creatingRecord).toEqual(true);
+        inst.cancelRecordCreation();
+        expect(inst.state.creatingRecord).toEqual(false);
+    })
+
+    it('should be able to generate record elements', () => {
+        const inst = _wrapper.instance();
+        const generatedRecords = inst.generateRecords(actionRecords);
+        expect(generatedRecords.length).toBeGreaterThan(0);
+        expect(generatedRecords.length).toEqual(2);
+    });
+});
