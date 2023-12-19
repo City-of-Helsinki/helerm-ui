865db954aa213d1fcec0ed0913b859700dd9a40a
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-12-19 14:21:35 +0200

    chore: initial e2e tests

---
+++ playwright.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from '@playwright/test';
+
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './e2e/tests',
+  timeout: 180 * 1000,
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  workers: 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['junit', { outputFile: 'test-results/e2e-junit-results.xml' }],
+    ['html']
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    actionTimeout: 30 * 1000,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: process.env.TEST_BASEURL ?? "https://tiedonohjaus-ui.dev.hel.ninja/",
+    ignoreHTTPSErrors: true,
+    screenshot: {
+      fullPage: true,
+      mode: "only-on-failure"
+    },
+    /* 'on-first-retry' Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+
+    // 'on-first-retry' https://playwright.dev/docs/videos
+    video: 'on-first-retry', 
+    contextOptions: {recordVideo: { dir: "./test-results/videos/"}}
+  },
+
+  projects: [
+    {
+      name: 'logged-out',
+      testMatch: [/pages/],
+    }
+  ],
+});
