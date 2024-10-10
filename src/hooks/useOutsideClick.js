b77dccb67a6a1380686a15426ce8bed1c51312b3
mikkojamG <mikko.jamia@gofore.com>
2024-10-10 12:30:54 +0300

    feat: faceted search helpers to functions TIED-164

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,29 @@
+import { useEffect, useRef, useState } from 'react';
+
+const useOutsideClick = (showInitially = false) => {
+  const ref = useRef(null);
+  const [show, setShow] = useState(showInitially);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      event.stopPropagation();
+
+      if (ref.current && !ref.current.contains(event.target)) {
+        setShow(false);
+      }
+    };
+
+    document.addEventListener('mouseup', handleClickOutside);
+    document.addEventListener('touchend', handleClickOutside);
+
+
+    return () => {
+      document.removeEventListener('mouseup', handleClickOutside);
+      document.removeEventListener('touchend', handleClickOutside);
+    };
+  }, []);
+
+  return { show, setShow, ref };
+};
+
+export default useOutsideClick;
