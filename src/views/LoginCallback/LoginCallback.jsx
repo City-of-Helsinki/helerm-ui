600c84fcac18edd9bca39b9209368f78215fe335
Jori Lindell <jori.lindell@gmail.com>
2026-06-05 12:29:26 +0300

    fix(login-callback): ignore HANDLING_LOGIN_CALLBACK callback error
    Filter out the known HDS SIGNIN_ERROR in LoginCallback onError:
    "Current state (HANDLING_LOGIN_CALLBACK) cannot be handled by a
    callback".
    
    Refs: TIED-241

--- src/views/LoginCallback/LoginCallback.jsx
+++ src/views/LoginCallback/LoginCallback.jsx
@@ -32,7 +32,32 @@ const LoginCallback = () => {
     navigate('/');
   };
 
-  const onError = () => {
+  const onError = (error) => {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    if (!error) return;
+    if (
+      error.type === 'SIGNIN_ERROR' &&
+      error.message ===
+        'Current state (HANDLING_LOGIN_CALLBACK) cannot be handled by a callback'
+    ) {
+      // Note: When the HDS team has improved the error handling,
+      // this should be doable with;
+      // ```
+      // if (isHandlingLoginCallbackError(error)) {
+      ///...
+      // }
+      // ```
+
+      // The HANDLING_LOGIN_CALLBACK error is raised only
+      // when we are using the <React.Strict>.
+      // We don't want to handle this error,
+      // because it is raised during every login process,
+      // and it does not affect any how to the signin process.
+      // When the <BrowserApp> is removed from the React.Strict -container,
+      // The error won't be raised anymore. The HDS team has been noted about this issue.
+      return;
+    }
     dispatch(handleLoginCallbackErrorThunk());
 
     addNotification({ label: 'Virhe', children: 'Kirjautuminen epäonnistui!', type: 'error' });
