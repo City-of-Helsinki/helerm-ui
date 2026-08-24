de5aa9cfe9f2a16a9d94ad96158e377a3280ce51
mikkojamG <mikko.jamia@gofore.com>
2026-08-24 15:06:19 +0300

    ci: fix e2e tests Refs: TIED-297

--- e2e/tests/pages/frontpage-spec.js
+++ e2e/tests/pages/frontpage-spec.js
@@ -31,19 +31,19 @@ test.describe('Frontpage', () => {
   test('Open links', async () => {
     await page.getByRole('button', { name: '14 Elinkeino- ja työvoimapalvelut' }).click();
     await page.getByRole('button', { name: '13 Tilastointi-, tutkimus- ja' }).click();
-    await page.getByRole('button', { name: 'Opetus- ja sivistystoimi  +' }).click();
-    await page.getByRole('button', { name: 'Ympäristöasiat  +' }).click();
+    await page.getByRole('button', { name: 'Opetus- ja sivistystoimi' }).click();
+    await page.getByRole('button', { name: 'Ympäristöasiat' }).click();
     await page.getByRole('button', { name: '10 Maankäyttö, rakentaminen' }).click();
     await page.getByRole('button', { name: '09 Turvallisuus ja yleinen jä' }).click();
-    await page.getByRole('button', { name: 'Liikenne  +' }).click();
-    await page.getByRole('button', { name: 'Tiedon hallinta  +' }).click();
-    await page.getByRole('button', { name: '06 Terveydenhuolto  +' }).click();
-    await page.getByRole('button', { name: 'Sosiaalitoimi  +' }).click();
+    await page.getByRole('button', { name: 'Liikenne' }).click();
+    await page.getByRole('button', { name: 'Tiedon hallinta' }).click();
+    await page.getByRole('button', { name: '06 Terveydenhuolto' }).click();
+    await page.getByRole('button', { name: 'Sosiaalitoimi' }).click();
     await page.getByRole('button', { name: '04 Kansainvälinen toiminta ja' }).click();
     await page.getByRole('button', { name: '03 Lainsäädäntö ja lainsäädä' }).click();
     await page.getByRole('button', { name: '02 Talousasiat, verotus ja' }).click();
-    await page.getByRole('button', { name: 'Henkilöstöasiat  +' }).click();
-    await page.getByRole('button', { name: 'Hallintoasiat  +' }).click();
+    await page.getByRole('button', { name: 'Henkilöstöasiat' }).click();
+    await page.getByRole('button', { name: 'Hallintoasiat' }).click();
   });
 
   test('Filter', async () => {
